162c0f2a5f5732d051d4ea90dd215d9023d88916
Thiago <thiago.t.cezar@gmail.com>
2020-01-13 10:24:50 -0300

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by K2 - Thiago Cézar on 13/01/20.
+//  Copyright © 2020 Thiago Cézar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
