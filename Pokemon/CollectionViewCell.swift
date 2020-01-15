dcefc18470f1fb5652bbaccd5fc3ac3fe15e1349
Thiago <thiago.t.cezar@gmail.com>
2020-01-15 14:05:16 -0300

    Create CollectionView and Properties for cell constraints

---
+++ Pokemon/CollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionViewCell.swift
+//  Pokemon
+//
+//  Created by K2 - Thiago Cézar on 15/01/20.
+//  Copyright © 2020 Thiago Cézar. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+   // @IBOutlet weak var imagePokemon: UIImageView!
+    
+    
+    @IBOutlet weak var namePokemon: UILabel!
+    
+    func Teste(){
+        //self.namePokemon.text = pokemons
+        
+    }
+    
+    
+}
--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let pokemons = ["Pikachu", "Raichu", "Bulbasaur", "Blastoise", "Charizard", "Charmander"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // Do any additional setup after loading the view
+        
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+            cell.namePokemon.text = pokemons[indexPath.item]
+            return cell
+    }
+    
 }
+
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let w = self.view.bounds.width
+        let cellW = w / 3 - 10
+        return CGSize(width: cellW, height: cellW)
+    }
+    
+}
+
+
 
